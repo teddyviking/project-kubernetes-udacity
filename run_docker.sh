f4a88c881d45dd50731d679f78a44eaad2334a0d
teddyviking <rtejado90@gmail.com>
2020-07-09 18:41:36 +0200

    Task 2: run container

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+docker build --tag=teddyviking/kubernetes_project .
 
-# Step 1:
-# Build image and add a descriptive tag
+docker image ls
 
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
+docker run  -p 8000:80 teddyviking/kubernetes_project
