2ef2303a7809acedbee9b9f340a11e9189591aa5
teddyviking <rtejado90@gmail.com>
2020-07-09 18:56:57 +0200

    Task 4: Upload to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,15 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+export DOCKER_ID_USER="teddyviking"
+export REPOSITORY="kubernetes_project"
 
-# Step 2:  
-# Authenticate & tag
+dockerpath=$DOCKER_ID_USER/kubernetes_project
+
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $REPOSITORY $DOCKER_ID_USER/$REPOSITORY 
 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKER_ID_USER/$REPOSITORY  
